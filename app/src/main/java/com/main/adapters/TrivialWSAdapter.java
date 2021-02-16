a7ed0314989e9f3bbabf0295e37365d864321b79
adriani94bil <adrian.ruiz.donado@gmail.com>
2021-02-16 13:13:31 +0100

    #1 Basic construct

---
+++ app/src/main/java/com/main/adapters/TrivialWSAdapter.java
@@ -0,0 +1,78 @@
+package com.main.adapters;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.RadioButton;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.main.MantenimientoActivity;
+import com.main.R;
+import com.main.modelo.Pregunta;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TrivialWSAdapter extends RecyclerView.Adapter<TrivialWSAdapter.MiViewHolder> {
+
+    private List<Pregunta> listaPreguntas;
+    private MantenimientoActivity mtoActivity;
+
+    public TrivialWSAdapter(List<Pregunta> preguntas) {
+
+        //llama al web services para pedirle las preguntas
+
+        if (preguntas==null){
+            this.listaPreguntas=new ArrayList<>();
+        }else{
+            this.listaPreguntas=preguntas;
+        }
+
+    }
+
+    @NonNull
+    @Override
+    public MiViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater= LayoutInflater.from(parent.getContext());
+        // la plantilla de cada item va a ser item_pregunta
+        View view= inflater.inflate(R.layout.item_pregunta_juego, parent, false);
+        TrivialWSAdapter.MiViewHolder miViewHolder=new TrivialWSAdapter.MiViewHolder(view);
+        return miViewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MiViewHolder holder, int position) {
+        final Pregunta pregunta=listaPreguntas.get(position);
+        holder.textViewId.setText(""+pregunta.getId());
+        holder.textViewPreg.setText(pregunta.getDescripcion());
+        //Falta el onclick
+        //holder.btnPista.
+    }
+
+    @Override
+    public int getItemCount() {
+        return listaPreguntas.size();
+    }
+
+    public class MiViewHolder extends RecyclerView.ViewHolder{
+        TextView textViewId;
+        TextView textViewPreg;
+        ImageButton btnPista;
+        RadioButton radioButtonTrue;
+        RadioButton radioButtonFalse;
+
+        public MiViewHolder(@NonNull View itemView) {
+            super(itemView);
+            textViewId=itemView.findViewById(R.id.textViewId);
+            textViewPreg=itemView.findViewById(R.id.textViewPregunta);
+            btnPista=itemView.findViewById(R.id.btnPista);
+            radioButtonTrue=itemView.findViewById(R.id.radioButtonTrue);
+            radioButtonFalse=itemView.findViewById(R.id.radioButtonFalse);
+
+        }
+    }
+}
---
+++ app/src/main/java/com/main/modelo/Pregunta.java
@@ -0,0 +1,65 @@
+package com.main.modelo;
+
+public class Pregunta {
+    public int id;
+    public String descripcion;
+    public String mensaje;
+    public boolean respuesta;
+
+    // atributo para guardar la respuesta del usuario
+
+    private boolean respuestaJugador;
+
+    public Pregunta(int id, String descripcion, boolean isCorrecto, String mensaje) {
+        this.id = id;
+        this.descripcion = descripcion;
+        this.respuesta = isCorrecto;
+        this.mensaje = mensaje;
+    }
+
+    public Pregunta(String descripcion,  String mensaje, boolean isCorrecto) {
+        this.descripcion = descripcion;
+        this.respuesta = isCorrecto;
+        this.mensaje = mensaje;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public boolean isRespuesta() {
+        return respuesta;
+    }
+
+    public void setRespuesta(boolean correcto) {
+        respuesta = correcto;
+    }
+
+    public String getMensaje() {
+        return mensaje;
+    }
+
+    public void setMensaje(String mensaje) {
+        this.mensaje = mensaje;
+    }
+
+    public boolean isRespuestaJugador() {
+        return respuestaJugador;
+    }
+
+    public void setRespuestaJugador(boolean respuestaJugador) {
+        this.respuestaJugador = respuestaJugador;
+    }
+}
